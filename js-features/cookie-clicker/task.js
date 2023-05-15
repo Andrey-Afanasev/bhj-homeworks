1c74641eed2e56726f855ebfd74285dcfc8bf81a
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-05-15 22:59:37 +0700

    15,05

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -6,8 +6,8 @@ const clicker = document.getElementById('clicker__counter');
 cookie.onclick = () => {
 
     clicker.textContent = +clicker.textContent + 1;
-    
-    if (cookie.width = 200) {
+
+    if (cookie.width === 200) {
         cookie.width = 250;
     } else {
         cookie.width = 200;
