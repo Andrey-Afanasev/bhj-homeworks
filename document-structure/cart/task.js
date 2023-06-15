1f6bc10de6e72f74939ae57ee36e3108d687fa55
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-06-15 21:46:00 +0700

    15,06

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -4,7 +4,7 @@ const minus = document.querySelectorAll('.product__quantity-control_dec')
 const plus = document.querySelectorAll('.product__quantity-control_inc')
 const card = document.querySelector('.cart__products')
 const pimage = document.querySelectorAll('.product__image')
-const addProducts = document.querySelectorAll('.cart__product')
+let addProducts
 const product = document.querySelectorAll('.product')
 
 
@@ -34,96 +34,67 @@ minus.forEach((e) => e.addEventListener('click', minusuem ))
 
 
 
-///////////////////////////////////
-
-
-
-
-
-
 /////////////////////////////////// добавляем в корзину
 
 
 
-function qwe(event) {
-
-    let index = event.target.closest('.product').getAttribute('data-id');
-
-    const prodCount = document.querySelectorAll('.cart__product-count')
-   
-
-    prodCount.forEach((e) => {
-
-        if (e.closest('.cart__product').getAttribute('data-id') !== index) {
-
-            console.log('xvxd')
-            return
-
-        }
-
-       })
-        
-    
-   
-        
-
-    
-   /* if (document.querySelectorAll('.cart__product-count')){
-
-        if(document.querySelectorAll('.cart__products')[0].getAttribute('data-id') !== (+index - 1)){
-
-            console.log(document.querySelectorAll('.cart__product-count')[0].closest(".cart__product").getAttribute('data-id'))
-            quantity[+index - 1].textContent = 1
-            return
-
-        }
-        
-    }*/
-
-
-    
-
-
-    const pf = document.createElement('div')
-    const kol = document.createElement('div')
-
-
-    const prod = product[+index - 1].cloneNode(false)
-    card.insertBefore(prod, card.firstElementChild)
-    prod.classList.remove('product')
-    prod.classList.add('cart__product')
+function addToCart(event) {
 
    
-    const ixmenaQuant = quantity[+index - 1].cloneNode(true)
-    ixmenaQuant.classList.remove('product__quantity-value')
-    ixmenaQuant.classList.add('cart__product-count')
 
-    pf.innerHTML = pimage[+index - 1].outerHTML
-    kol.innerHTML =  ixmenaQuant.outerHTML
+      let index = event.target.closest('.product').getAttribute('data-id');
 
 
-    prod.insertBefore(pf, prod.fistElementChild)
-    prod.insertBefore(kol, prod.firstElementChild)
+        if ([...document.querySelectorAll('.cart__product')].some(e => e.getAttribute('data-id') === index ) === true ) {
+
+        let elementKorzin = [...document.querySelectorAll('.cart__product')].findIndex(e => e.getAttribute('data-id') == index)
+
+        document.querySelectorAll('.cart__product-count')[elementKorzin].textContent = +document.querySelectorAll('.cart__product')[elementKorzin].textContent + +quantity[+index - 1].textContent
+
+        quantity[+index - 1].textContent = 1
+
+    
+
+      } else {
 
 
+        const pf = document.createElement('div')
+        const kol = document.createElement('div')
 
-    quantity[+index - 1].textContent = 1
+        const prod = product[+index - 1].cloneNode(false)
+        card.insertBefore(prod, card.lasttElementChild)
+        prod.classList.remove('product')
+        prod.classList.add('cart__product')
+
+    
+        const ixmenaQuant = quantity[+index - 1].cloneNode(true)
+        ixmenaQuant.classList.remove('product__quantity-value')
+        ixmenaQuant.classList.add('cart__product-count')
+
+        pf.innerHTML = pimage[+index - 1].outerHTML
+        kol.innerHTML =  ixmenaQuant.outerHTML
+
+        prod.insertBefore(pf, prod.fistElementChild)
+        prod.insertBefore(kol, prod.firstElementChild)
+
+
+        quantity[+index - 1].textContent = 1
+
+      }
+
+
+    } 
 
     
 
 
-    
-}
-
-
-productadd.forEach((e) => e.addEventListener('click', qwe))
-
-
-////////////////////////////////////
-
-
-////////////+document.querySelectorAll('.cart__product')[0].getAttribute('data-id')
+productadd.forEach((e) => e.addEventListener('click', addToCart))
 
 
 
-///document.querySelectorAll('.cart__product-count')[+index - 1].textContent =  +document.querySelectorALL('.cart__product-count')[+index - 1].textContent + +quantity[+index - 1].textContent
+
+
+
+
+
+
