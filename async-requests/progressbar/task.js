d36f361cb14693d937734e3f7ddc3024b5c2a8f0
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-06-16 22:40:49 +0700

    16,06

--- async-requests/progressbar/task.js
+++ async-requests/progressbar/task.js
@@ -1,6 +1,37 @@
-let formData = new FormData()
+
+const form = document.getElementById('form');
+const progress = document.getElementById('progress');
+const file = document.getElementById('file');
 
 
-let xhr = new XMLHttpRequest()
+const submitForms = (e) => {
 
-xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
+    e.preventDefault()
+
+    let xhr = new XMLHttpRequest()
+
+    const subEvent = () => {
+
+        if(xhr.readyState === xhr.DONE) {
+
+        }
+
+    }
+
+    xhr.addEventListener('readystatechange', subEvent)
+
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/upload')
+
+    const formData = new FormData(form)
+
+    xhr.send(formData)
+
+
+}
+
+form.addEventListener('submit', submitForms)
+
+
+
+
+
