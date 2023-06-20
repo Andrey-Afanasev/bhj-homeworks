1700f123c028542ea0e9727dfd3f2aed9dff4742
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-06-20 07:52:59 +0700

    19,06

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -2,14 +2,19 @@
 const editor = document.getElementById('editor')
 const storageName = localStorage.getItem('value')
 
-if (storageName ) {
-
+if (storageName) {
     editor.value = storageName
 
 } else {
-
     localStorage.setItem('value', editor.value)
-
 }
+
+
+qwe = () => {
+    localStorage.removeItem('value')
+    localStorage.setItem('value', editor.value)
+}
+
+editor.addEventListener('change', qwe)
 
 
