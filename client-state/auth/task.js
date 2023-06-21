3562fab2f9e0f19994ec80025e84a67f32826eb5
Andrey Petrovich <andrey_petrovicha@mail.ru>
2023-06-21 08:28:04 +0700

    21,06

--- client-state/auth/task.js
+++ client-state/auth/task.js
@@ -1,33 +1,15 @@
-const login = document.getElementsByName('login')
-const password = document.getElementsByName('password')
+
 const btn = document.querySelector('.btn')
 const form = document.getElementById('signin__form')
+const login = document.querySelector('input[type="text"]')
+const password = document.querySelector('input[type="password"]')
 
+const dataObj = {
+    login: login.value,
+    password: password.value
+  }
 
 let xhr = new XMLHttpRequest()
-
-
-/*
-const user = {
-    id: 1,
-    login: 'Demo',
-    password: 'Demo',
-}
-
-function sevObjeckt(key, object) {
-    localStorage.setItem(key, JSON.stringify(object))
-
-}
-
-sevObjeckt('user', user)
-
-function restoreObject(key) {
-    return JSON.parse(localStorage.getItem(key))
-}
-
-*/
-
-
 
 
 const submitForms = (e) => {
@@ -39,28 +21,20 @@ const submitForms = (e) => {
     const subEvent = () => {
 
         if(xhr.readyState === xhr.DONE) {
-
+            console.log('svd')
         }
 
     }
 
-
-
     xhr.addEventListener('readystatechange', subEvent)
-    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
 
+
+    xhr.open('POST', 'https://students.netoservices.ru/nestjs-backend/auth')
     const formData = new FormData(form)
     xhr.send(formData)
 
     form.reset()
 
-    xhr.responseType = 'json'
-    xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/auth')
-    xhr.send()
-
-    if(xhr.readyState === xhr.DONE) {
-        console.log(xhr.respons)
-    }
 
 }
 
@@ -71,5 +45,4 @@ const submitForms = (e) => {
 
 form.addEventListener('submit', submitForms)
 
-JSON.stringify
-JSON.parse
+xhr.parseResponseBody(response)
